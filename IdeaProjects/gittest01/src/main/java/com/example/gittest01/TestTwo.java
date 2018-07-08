1adddbae4d32353b615ada4274c268d7e13dd05c
huyinsen <978411665@qq.com>
2018-07-08 11:02:24 +0800

    第二次增加测试

---
+++ IdeaProjects/gittest01/src/main/java/com/example/gittest01/TestTwo.java
@@ -0,0 +1,23 @@
+package com.example.gittest01;
+
+public class TestTwo {
+    private int id;
+
+    private int c;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+}
