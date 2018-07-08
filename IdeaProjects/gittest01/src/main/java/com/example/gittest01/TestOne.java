df3874e8479b27a51d33f11202bca97fb5eb9851
huyinsen <978411665@qq.com>
2018-07-08 10:05:13 +0800

    第二次测试

---
+++ IdeaProjects/gittest01/src/main/java/com/example/gittest01/TestOne.java
@@ -0,0 +1,22 @@
+package com.example.gittest01;
+
+public class TestOne {
+    private int id;
+    private int b;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
